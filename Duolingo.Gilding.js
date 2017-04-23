354a6710addd942a3caf875d6a7f092945dea939
Max G <maxgarber@users.noreply.github.com>
2017-04-23 12:05:10 -0400

    added Duolingo.gilding.js
    scroll to first un-gilded skill

---
+++ Duolingo.Gilding.js
@@ -0,0 +1,14 @@
+//	Duolingo.Gilding.js
+//		scroll to first un-gilded skill
+//	v1.0	[√] tested on 2017/04/23
+//	© 2017 Max Garber <max.garber+dev@gmail.com>
+
+javascript:(function(){
+	var u = document.getElementsByClassName('unlocked');
+	window.scrollTo(0,u[u.0].offsetTop-100);
+});
+
+//	encoded output: (converted by http://mrcoles.com/bookmarklet/)
+javascript:(function()%7Bjavascript%3A(function()%257Bjavascript%253A(function()%257Bvar%2520u%2520%253D%2520document.getElementsByClassName('unlocked')%253Bwindow.scrollTo(0%252Cu%255Bu.length-1%255D.offsetTop-100)%253B%257D)%257D)()%7D)()
+
+// EOF
