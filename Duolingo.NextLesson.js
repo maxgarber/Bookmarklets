f3f3acd3fcd7021b64e008c136bd6af3125a0d60
Max Garber <max.garber+dev@gmail.com>
2017-04-23 13:03:47 -0400

    more formatting fixes
    no more web file editor, going direct from now on

--- Duolingo.NextLesson.js
+++ Duolingo.NextLesson.js
@@ -3,11 +3,11 @@
 //	v1.0	[√] tested on 2017/04/23
 //	© 2017 Max Garber <max.garber+dev@gmail.com>
 
-//  raw javascript block -- block enclosures omitted
+// raw javascript block -- block enclosures omitted
 var n=document.getElementsByClassName('unlocked');
 window.scrollTo(0,(n[n.length-1]).offsetTop);
 
-//  encoded output: (converted by http://mrcoles.com/bookmarklet/)
+//	encoded output: (converted by http://mrcoles.com/bookmarklet/)
 javascript:(function()%7Bvar%20n%3Ddocument.getElementsByClassName('unlocked')%3Bwindow.scrollTo(0%2C(n%5Bn.length-1%5D).offsetTop)%7D)()
 
-//  EOF
+// EOF
